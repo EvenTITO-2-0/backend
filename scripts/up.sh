418cb7f360bb299103f4fead00c1c05c820c598e
mateocapon <mcapon@fi.uba.ar>
2025-03-27 19:48:46 -0300

    make setup easier

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 NETWORK_NAME="eventito-dev-network"
 
+# Check if .env file exists, if not create it from .env.example
+if [ ! -f .env ]; then
+    echo "Creating .env file from .env.example..."
+    cp .env.example .env
+fi
+
 if ! docker network ls --format '{{.Name}}' | grep -wq "$NETWORK_NAME"; then
     echo "Creating network '$NETWORK_NAME'..."
     docker network create "$NETWORK_NAME"
