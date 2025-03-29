af1d733014894c32a8e811f61dc1bd2773c68eaf
mateocapon <mcapon@fi.uba.ar>
2025-03-29 20:00:45 -0300

    more fixes

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,11 +1,13 @@
-from typing import Type
+from typing import Type, TypeVar
 
 from app.database.session_dep import SessionDep
 from app.repository.crud_repository import Repository
 
+T = TypeVar("T", bound="Repository")
 
-def get_repository(repository: Type[Repository]):
+
+def get_repository(repository: Type[T], *args):
     async def _get_repository(session: SessionDep):
-        return repository(session)
+        return repository(session, *args)
 
     return _get_repository
